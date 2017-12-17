aaf0016cdb45c02711275626e17e8f838887e92c
guan <1027526351@qq.com>
2017-12-17 10:58:23 +0800

    将base.less和index.less转成了rem的形式,base1.less,index1.less是rem的文件

--- js/common.js
+++ js/common.js
@@ -1,7 +1,8 @@
 
 $(function(){
 	mui('.mui-scroll-wrapper').scroll({
-		deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+		deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+		indicators: false
 	});
 
 	//给所有页面顶部的返回图标添加返回到上一页的功能
