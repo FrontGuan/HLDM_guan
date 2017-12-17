6ef863fb17437b0b7419786180b1f39b27aa751b
guan <1027526351@qq.com>
2017-12-17 15:58:47 +0800

    用了两种方式实现用rem适配屏幕尺寸的问题,一种是在common.js改变html根节点的fontSize大小,另一种是先按375尺寸写好的px再用转换器转换成适配每个尺寸rem,到此.整个项目完成

--- js/common.js
+++ js/common.js
@@ -1,5 +1,13 @@
 
 $(function(){
+	//获取屏幕的宽
+    var window_width = $(window).width();
+    console.log(window_width);
+    var html_size = (window_width / 375) * 100 ;
+    //设置html跟结点的fontSize大小
+    document.querySelector('html').style.fontSize = html_size + 'px';
+    // console.log(document.querySelector('html').style.fontSize);
+
 	mui('.mui-scroll-wrapper').scroll({
 		deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
 		indicators: false
